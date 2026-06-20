f592993648fb82bfbb7e1b182af901892b45e18c
Monica <jay.m.howard@gmail.com>
2026-06-20 11:34:00 -0500

    added an About.jsx page

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Us</h1>
+            <p>Welcome to our website! We are passionate about providing high-quality products and excellent customer service. Our team is dedicated to ensuring that you have the best shopping experience possible. Thank you for choosing us!</p>
+        </div>
+    );
+};
+
+export default About;
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <nav>
+        <Link href="#About">About</Link>
+      </nav>
       <h1>AMTS</h1>
     </>
   )
